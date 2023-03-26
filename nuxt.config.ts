68d9098b35efa4225cbc02d65e38abbdd6fa03ec
Blechlawine <marczinser@gmx.de>
2023-03-26 15:47:06 +0200

    fix: fix shynet script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,5 @@ export default defineNuxtConfig({
     telemetry: false,
     app: {
         baseURL: process.env.NODE_ENV === "production" ? "/technologies/" : "/",
-        head: {
-            script: [
-                {
-                    key: "shynet",
-                    src: "https://shynet.zmarc.de/ingress/d9ed8937-cfc3-4482-a0be-955ed70dde0e/script.js",
-                    defer: true,
-                },
-            ],
-        },
     },
 });
---
+++ plugins/shynet.client.ts
@@ -0,0 +1,66 @@
+export default defineNuxtPlugin((nuxtApp) => {
+    // This is a lightweight and privacy-friendly analytics script from Shynet, a self-hosted
+    // analytics tool. To give you full visibility into how your data is being monitored, this
+    // file is intentionally not minified or obfuscated. To learn more about Shynet (and to view
+    // its source code), visit <https://github.com/milesmcc/shynet>.
+    //
+    // This script only sends the current URL, the referrer URL, and the page load time. That's it!
+
+    var Shynet = {
+        idempotency: null as string | null,
+        heartbeatTaskId: null as NodeJS.Timer | null,
+        skipHeartbeat: false,
+        sendHeartbeat: function () {
+            try {
+                if (document.hidden || Shynet.skipHeartbeat) {
+                    return;
+                }
+
+                Shynet.skipHeartbeat = true;
+                var xhr = new XMLHttpRequest();
+                xhr.open(
+                    "POST",
+                    "https://shynet.zmarc.de/ingress/d9ed8937-cfc3-4482-a0be-955ed70dde0e/script.js",
+                    true,
+                );
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.onload = function () {
+                    Shynet.skipHeartbeat = false;
+                };
+                xhr.onerror = function () {
+                    Shynet.skipHeartbeat = false;
+                };
+                xhr.send(
+                    JSON.stringify({
+                        idempotency: Shynet.idempotency,
+                        referrer: document.referrer,
+                        location: window.location.href,
+                        loadTime:
+                            window.performance.timing.domContentLoadedEventEnd -
+                            window.performance.timing.navigationStart,
+                    }),
+                );
+            } catch (e) {}
+        },
+        newPageLoad: function () {
+            if (Shynet.heartbeatTaskId != null) {
+                clearInterval(Shynet.heartbeatTaskId);
+            }
+            Shynet.idempotency =
+                Math.random().toString(36).substring(2, 15) +
+                Math.random().toString(36).substring(2, 15);
+            Shynet.skipHeartbeat = false;
+            Shynet.heartbeatTaskId = setInterval(Shynet.sendHeartbeat, parseInt("5000"));
+            Shynet.sendHeartbeat();
+        },
+    };
+
+    // window.addEventListener("load", Shynet.newPageLoad);
+
+    nuxtApp.hook("page:transition:finish", (pageComponent) => {
+        console.log("page:transition:finish", pageComponent);
+        if (Shynet) {
+            Shynet.newPageLoad();
+        }
+    });
+});
